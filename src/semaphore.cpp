00bc7f367d9dd302f909a4bd83b7faf09d14806d
Benjamin 'Albsi' Albsmeier <albsmeier@gmail.com>
2014-06-27 04:18:56 +0200

    semaphore

---
+++ src/semaphore.cpp
@@ -0,0 +1,27 @@
+#include "semaphore.hpp"
+#include <mutex>
+
+semaphore::semaphore() {
+    this->counter = 0;
+}
+
+semaphore::semaphore(unsigned int start_value) {
+    this->counter = start_value;
+}
+
+void semaphore::wait() {
+    while(this->counter != 0) {
+    }
+}
+
+void semaphore::increment() {
+    mtx.lock();
+    this->counter++;
+    mtx.unlock();
+}
+
+void semaphore::decrement() {
+    mtx.lock();
+    this->counter--;
+    mtx.unlock();
+}
